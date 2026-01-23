1556bfacca4163d26252fb79fa8cf7417dea2a9a
ETech Latop 4 bryanR <etechchargers5298@gmail.com>
2026-01-23 14:14:24 -0500

    Messed up changes

--- src/main/java/frc/robot/utils/RobotContainer.java
+++ src/main/java/frc/robot/utils/RobotContainer.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc.robot.utils;
 
 import static edu.wpi.first.units.Units.*;
 
@@ -15,8 +15,6 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-
-import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class RobotContainer {
