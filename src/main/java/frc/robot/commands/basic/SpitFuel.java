533f22d77518688552c82a3cea553d1688b370e5
TylerKeating143 <tylerwboomboom@gmail.com>
2026-01-29 17:06:11 -0500

    added spit command

---
+++ src/main/java/frc/robot/commands/basic/SpitFuel.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.basic;
+
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class SpitFuel extends Command {
+  @SuppressWarnings("PMD.UnusedPrivateField")
+  private final Intake intake;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public SpitFuel() {
+    intake = Intake.getInstance();
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    intake.retract();
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+    
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
