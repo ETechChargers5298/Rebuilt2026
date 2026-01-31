03473549b8a0878cea1eefade3ba6859936d9ba3
J.A.Bianchi <JABianchi@gmail.com>
2026-01-31 17:42:58 -0500

    Added "SwerveBus" as name of CANivore's network

--- src/main/java/frc/robot/utils/TunerConstants.java
+++ src/main/java/frc/robot/utils/TunerConstants.java
@@ -88,7 +88,7 @@ public class TunerConstants {
 
     // CAN bus that the devices are located on;
     // All swerve devices must share the same CAN bus
-    public static final CANBus CAN_DRIVE_BUS = new CANBus("", "./logs/example.hoot");
+    public static final CANBus CAN_DRIVE_BUS = new CANBus("SwerveBus", "./logs/example.hoot");
 
     // Theoretical free speed (m/s) at 12 V applied output;
     // This needs to be tuned to your individual robot
