f45c83c0f68625abafd29c88b8cf2a868ec0b865
TylerKeating143 <tylerwboomboom@gmail.com>
2026-02-04 16:21:13 -0500

    LED ROLES

--- src/main/java/frc/robot/subsystems/LEDstrip.java
+++ src/main/java/frc/robot/subsystems/LEDstrip.java
@@ -7,5 +7,28 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDstrip {
-    
+    // ON OR OFF
+    public static final double DISABLED = LEDColors.BLACK;
+    public static final double ENABLED = LEDColors.BLUE;
+
+    //SCORER
+    public static final double IN_SCORER = LEDColors.GREEN;
+   public static final double SCORE_READY = LEDColors.RAINBOW_BPM;
+
+    //INTAKE
+    public static final double INTAKING = LEDColors.YELLOW;
+    public static final double STUCK = LEDColors.RED;
+    public static final double FULL = LEDColors.GOLD;
+    public static final double FUEL_STUCK = LEDColors.RED_HEARTBEAT;
+
+    //HOPPER
+    //public static final double 
+
+
+    //
+    public static final double AUTO = LEDColors.VIOLET;
+
+
+
+
 }
