37a8336f04f735d1d29f9eddf417f2682b28fae0
Daniel Zou <danielzou.sia@gmail.com>
2026-02-12 17:47:05 -0500

    Pigeon config + elastic layout

--- src/main/java/frc/robot/utils/TunerConstants.java
+++ src/main/java/frc/robot/utils/TunerConstants.java
@@ -84,7 +84,13 @@ public class TunerConstants {
     private static final CANcoderConfiguration encoderInitialConfigs = new CANcoderConfiguration();
     
     // Configs for the Pigeon 2; leave this null to skip applying Pigeon 2 configs
-    public static final Pigeon2Configuration pigeonConfigs = null;
+    public static final Pigeon2Configuration pigeonConfigs = new Pigeon2Configuration()
+        .withMountPose(new MountPoseConfigs()
+            .withMountPosePitch(90)
+            .withMountPoseRoll(0)
+            .withMountPoseYaw(0)
+        );
+        
 
     // CAN bus that the devices are located on;
     // All swerve devices must share the same CAN bus
