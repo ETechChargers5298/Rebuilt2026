5543c06b9ed66c00815b6260916065b9cd3ee99e
TylerKeating143 <tylerwboomboom@gmail.com>
2026-03-04 16:44:06 -0500

    getting the turret angles

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -97,7 +97,11 @@ public class Turret extends SubsystemBase {
 
   // In-line Command to rotate the turret to a specific angle - in degrees
   public Command aimTurretToSetPointCommand( Supplier<Double> turretAngle) {
-
+      double turretDeg = getTurretAngle();
+      double targetRotations = (turretDeg / 360) * GEAR_RATIO;
+      double currentRotations = getTurretAngle();
+      double turretTurn = targetRotations - currentRotations;
+      aimTurret(0.5);
     return null;
   }
 
