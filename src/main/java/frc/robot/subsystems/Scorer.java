1082632764b369d75dfa55c7cb0c325dfd4bfbfb
moik-gamer <mooikeboi@gmail.com>
2026-03-04 16:34:12 -0500

    made some test code for getAnglerAngle & flywheelrpm

--- src/main/java/frc/robot/subsystems/Scorer.java
+++ src/main/java/frc/robot/subsystems/Scorer.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.RelativeEncoder;
+
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.FieldConstants;
@@ -21,6 +23,7 @@ public class Scorer {
     private double robotX = Drivetrain.getInstance().getRobotX();
     private double robotY = Drivetrain.getInstance().getRobotY();
     private double robotAngle = Drivetrain.getInstance().getRobotAngleDegrees();
+    
 
     // SCORER CONSTRUCTOR
     public Scorer(String side, double xOffset, double yOffset){
@@ -71,13 +74,60 @@ public class Scorer {
         return 180.0 - getAngleToHubFromRobotPerspective();
     }
 
-
+    
     // Method to be called once per scheduler run
     public void periodic() {
         SmartDashboard.putNumber("Distance To Hub", getDistanceToHub());
         SmartDashboard.putNumber("Angle To Hub - Turret", getAngleToHubFromTurretPerspective());
     }
 
+    //testing this for getAnglerAngle & flywheel rpm
+    public class ShotParameter {
+        public final double distance;
+        public final double rpm;
+        public final double angle;
 
+    public ShotParameter(double distance, double rpm, double angle) {
+        this.distance = distance;
+        this.rpm = rpm;
+        this.angle = angle;
+    }
+}
+
+public class ShooterInterpolation { 
+    // Your 6 data points sorted by distance
+    private final ShotParameter[] dataPoints = {
+        new ShotParameter(84.0, 4900, 45),
+        new ShotParameter(60.0, 4450, 40),
+        new ShotParameter(35.0, 4200, 35),
+        new ShotParameter(31.5, 4200, 30)
+        
+    };
+
+    public ShotParameter getIdealShot(double targetDistance) {
+        // 1. Handle edge cases (distance outside your measured range)
+        if (targetDistance <= dataPoints[0].distance) return dataPoints[0];
+        if (targetDistance >= dataPoints[dataPoints.length - 1].distance) 
+            return dataPoints[dataPoints.length - 1];
+
+        // 2. Find the two points we are between
+        for (int i = 0; i < dataPoints.length - 1; i++) {
+            ShotParameter p1 = dataPoints[i];
+            ShotParameter p2 = dataPoints[i + 1];
+
+            if (targetDistance >= p1.distance && targetDistance <= p2.distance) {
+                // 3. Calculate interpolation factor (0.0 to 1.0)
+                double t = (targetDistance - p1.distance) / (p2.distance - p1.distance);
+                
+                // 4. Interpolate RPM and Angle
+                double interpolatedRpm = p1.rpm + t * (p2.rpm - p1.rpm);
+                double interpolatedAngle = p1.angle + t * (p2.angle - p1.angle);
+
+                return new ShotParameter(targetDistance, interpolatedRpm, interpolatedAngle);
+            }
+        }
+        return dataPoints[0]; // Fallback
+    }
+}
 
 }
